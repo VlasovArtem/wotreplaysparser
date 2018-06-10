523af8c72152dbe37ad91d59293fb66c16a37d38
Vlasov Artem <avlasov@selectica.com>
2018-06-10 16:31:24 +0300

    Update return type of the method in the class ChampionshipStatisticUtils

--- src/main/java/org/avlasov/utils/statistic/ChampionshipStatisticUtils.java
+++ src/main/java/org/avlasov/utils/statistic/ChampionshipStatisticUtils.java
@@ -1,5 +1,6 @@
 package org.avlasov.utils.statistic;
 
+import lombok.Builder;
 import org.avlasov.entity.match.Match;
 import org.avlasov.entity.match.enums.DrawGroup;
 import org.avlasov.entity.statistic.*;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import static org.avlasov.utils.statistic.StatisticUtils.findBestByScore;
 import static org.avlasov.utils.statistic.StatisticUtils.findWorstByScore;
@@ -16,7 +19,7 @@ import static org.avlasov.utils.statistic.StatisticUtils.findWorstByScore;
  * Created By artemvlasov on 09/06/2018
  **/
 @Component
-public class ChampionshipStatisticUtils extends AbstractStatisticUtils<ChampionshipStatistic> {
+public class ChampionshipStatisticUtils extends AbstractStatisticUtils<Optional<ChampionshipStatistic>> {
 
     private final PlatoonStatisticUtils platoonStatisticUtils;
     private final VehicleStatisticUtils vehicleStatisticUtils;
@@ -26,23 +29,37 @@ public class ChampionshipStatisticUtils extends AbstractStatisticUtils<Champions
     private final MatchUtils matchUtils;
     private final DrawGroupPlayerStatisticUtils drawGroupPlayerStatisticUtils;
 
+    @Builder
     public ChampionshipStatisticUtils(PlatoonStatisticUtils platoonStatisticUtils, VehicleStatisticUtils vehicleStatisticUtils, PlayerStatisticUtils playerStatisticUtils, MatchStatisticUtils matchStatisticUtils, MatchesStatisticUtils matchesStatisticUtils, MatchUtils matchUtils, DrawGroupPlayerStatisticUtils drawGroupPlayerStatisticUtils) {
-        this.platoonStatisticUtils = platoonStatisticUtils;
-        this.vehicleStatisticUtils = vehicleStatisticUtils;
-        this.playerStatisticUtils = playerStatisticUtils;
-        this.matchStatisticUtils = matchStatisticUtils;
-        this.matchesStatisticUtils = matchesStatisticUtils;
-        this.matchUtils = matchUtils;
-        this.drawGroupPlayerStatisticUtils = drawGroupPlayerStatisticUtils;
+        this.platoonStatisticUtils = Objects.requireNonNull(platoonStatisticUtils);
+        this.vehicleStatisticUtils = Objects.requireNonNull(vehicleStatisticUtils);
+        this.playerStatisticUtils = Objects.requireNonNull(playerStatisticUtils);
+        this.matchStatisticUtils = Objects.requireNonNull(matchStatisticUtils);
+        this.matchesStatisticUtils = Objects.requireNonNull(matchesStatisticUtils);
+        this.matchUtils = Objects.requireNonNull(matchUtils);
+        this.drawGroupPlayerStatisticUtils = Objects.requireNonNull(drawGroupPlayerStatisticUtils);
     }
 
-    public ChampionshipStatistic calculateStatistic(List<Match> matches,
+    @Override
+    public Optional<ChampionshipStatistic> calculateStatistic(List<Match> matches) {
+        if (matches !=  null && !matches.isEmpty()) {
+            List<VehicleStatistic> vehicleStatistics = vehicleStatisticUtils.calculateStatistic(matches);
+            List<PlatoonStatistic> platoonStatistics = platoonStatisticUtils.calculateStatistic(matches);
+            List<PlayerStatistic> playerStatistics = playerStatisticUtils.calculateStatistic(matches);
+            MatchStatistic matchStatistic = matchStatisticUtils.calculateStatistic(matches).orElse(null);
+            Map<DrawGroup, List<DrawGroupPlayerStatistic>> drawGroupListMap = drawGroupPlayerStatisticUtils.collectDrawGroupPlayers(playerStatistics);
+            return calculateStatistic(matches, vehicleStatistics, platoonStatistics, playerStatistics, drawGroupListMap, matchStatistic);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<ChampionshipStatistic> calculateStatistic(List<Match> matches,
                                                                  List<VehicleStatistic> vehicleStatistics,
                                                                  List<PlatoonStatistic> platoonStatistics,
                                                                  List<PlayerStatistic> playerStatistics,
                                                                  Map<DrawGroup, List<DrawGroupPlayerStatistic>> drawGroupListMap,
                                                                  MatchStatistic matchStatistic) {
-        return ChampionshipStatistic.builder()
+        return Optional.ofNullable(ChampionshipStatistic.builder()
                 .matchesStatistic(matchesStatisticUtils.calculateStatistic(matches).orElse(null))
                 .bestMatch(matchUtils.findBestMatch(matches).orElse(null))
                 .worstMatch(matchUtils.findWorstMatch(matches).orElse(null))
@@ -54,16 +71,7 @@ public class ChampionshipStatisticUtils extends AbstractStatisticUtils<Champions
                 .worstPlayer(findWorstByScore(playerStatistics))
                 .bestVehicle(findBestByScore(vehicleStatistics))
                 .matchStatistic(matchStatistic)
-                .build();
+                .build());
     }
 
-    @Override
-    public ChampionshipStatistic calculateStatistic(List<Match> matches) {
-        List<VehicleStatistic> vehicleStatistics = vehicleStatisticUtils.calculateStatistic(matches);
-        List<PlatoonStatistic> platoonStatistics = platoonStatisticUtils.calculateStatistic(matches);
-        List<PlayerStatistic> playerStatistics = playerStatisticUtils.calculateStatistic(matches);
-        MatchStatistic matchStatistic = matchStatisticUtils.calculateStatistic(matches).orElse(null);
-        Map<DrawGroup, List<DrawGroupPlayerStatistic>> drawGroupListMap = drawGroupPlayerStatisticUtils.collectDrawGroupPlayers(playerStatistics);
-        return calculateStatistic(matches, vehicleStatistics, platoonStatistics, playerStatistics, drawGroupListMap, matchStatistic);
-    }
 }
